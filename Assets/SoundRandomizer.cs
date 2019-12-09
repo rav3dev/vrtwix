eae3afd7ea2068490cfbe32df6c05092801758ad
Helg Broforsky <Oyshoboy@gmail.com>
2019-12-09 02:47:36 -0800

    Sound Randomizer

---
+++ Assets/SoundRandomizer.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+
+public class SoundRandomizer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public void PlayOneShotAndRandomize(AudioClip Sound)
+    {
+        audioSource.pitch = Random.Range(0.9f, 1.1f);
+        audioSource.PlayOneShot(Sound);
+    }
+}
