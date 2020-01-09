edf2372dada5d4acc447868631db611a6ef2ffd7
Helg Broforsky <Oyshoboy@gmail.com>
2020-01-08 19:35:49 -0800

    Created test scene for driving

---
+++ Assets/_VRtwix/EasySuspenssion/EasySuspension.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode()]
+public class EasySuspension : MonoBehaviour {
+	[Range(0, 20)]
+	public float naturalFrequency = 10;
+
+	[Range(0, 3)]
+	public float dampingRatio = 0.8f;
+
+	[Range(-1, 1)]
+	public float forceShift = 0.03f;
+
+	public bool setSuspensionDistance = true;
+
+	void Update () {
+		// work out the stiffness and damper parameters based on the better spring model
+		foreach (WheelCollider wc in GetComponentsInChildren<WheelCollider>()) {
+			JointSpring spring = wc.suspensionSpring;
+
+			spring.spring = Mathf.Pow(Mathf.Sqrt(wc.sprungMass) * naturalFrequency, 2);
+			spring.damper = 2 * dampingRatio * Mathf.Sqrt(spring.spring * wc.sprungMass);
+
+			wc.suspensionSpring = spring;
+
+			Vector3 wheelRelativeBody = transform.InverseTransformPoint(wc.transform.position);
+			float distance = GetComponent<Rigidbody>().centerOfMass.y - wheelRelativeBody.y + wc.radius;
+
+			wc.forceAppPointDistance = distance - forceShift;
+
+			// the following line makes sure the spring force at maximum droop is exactly zero
+			if (spring.targetPosition > 0 && setSuspensionDistance)
+				wc.suspensionDistance = wc.sprungMass * Physics.gravity.magnitude / (spring.targetPosition * spring.spring);
+		}
+	}
+
+// uncomment OnGUI to observe how parameters change
+
+/*
+	public void OnGUI()
+	{
+		foreach (WheelCollider wc in GetComponentsInChildren<WheelCollider>()) {
+			GUILayout.Label (string.Format("{0} sprung: {1}, k: {2}, d: {3}", wc.name, wc.sprungMass, wc.suspensionSpring.spring, wc.suspensionSpring.damper));
+		}
+
+		var rb = GetComponent<Rigidbody> ();
+
+		GUILayout.Label ("Inertia: " + rb.inertiaTensor);
+		GUILayout.Label ("Mass: " + rb.mass);
+		GUILayout.Label ("Center: " + rb.centerOfMass);
+	}
+*/
+
+}
---
+++ Assets/_VRtwix/EasySuspenssion/RearWheelDrive.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RearWheelDrive : MonoBehaviour {
+
+	private WheelCollider[] wheels;
+
+	public float maxAngle = 30;
+	public float maxTorque = 300;
+	public GameObject wheelShape;
+    public ParticleSystem exhaust;
+    public GameObject engineModel;
+    public joystick jstk;
+    public steeringWheel sw;
+    //public acc
+    // here we find all the WheelColliders down in the hierarchy
+    public void Start()
+	{
+		wheels = GetComponentsInChildren<WheelCollider>();
+
+		for (int i = 0; i < wheels.Length; ++i) 
+		{
+			var wheel = wheels [i];
+
+			// create wheel shapes only when needed
+			if (wheelShape != null)
+			{
+				var ws = GameObject.Instantiate (wheelShape);
+				ws.transform.parent = wheel.transform;
+			}
+		}
+	}
+
+	// this is a really simple approach to updating wheels
+	// here we simulate a rear wheel drive car and assume that the car is perfectly symmetric at local zero
+	// this helps us to figure our which wheels are front ones and which are rear
+	public void Update()
+	{
+        float angle = maxAngle * (sw.angle / 720 );//Input.GetAxis("Horizontal");
+        float torque = maxTorque * jstk.value.y;//Input.GetAxis("Vertical");
+        float joystickValueNorm = Mathf.Abs(jstk.value.y);
+
+        // changing particles emissions 
+        var emission = exhaust.emission;
+        emission.rateOverTimeMultiplier = 5f + (joystickValueNorm * 20f);
+
+        // changing enginge shake
+        engineModel.GetComponent<Animator>().SetFloat("Engine_Speed", 1f + (joystickValueNorm * 5f));
+
+        // changing enginge sound
+        engineModel.GetComponent<AudioSource>().pitch = 0.5f + (joystickValueNorm * 0.5f);
+        engineModel.GetComponent<AudioSource>().volume = 0.17f + (joystickValueNorm * 0.2f);
+
+        foreach (WheelCollider wheel in wheels)
+		{
+			// a simple car where front wheels steer while rear ones drive
+			if (wheel.transform.localPosition.z > 0)
+				wheel.steerAngle = angle;
+
+			//if (wheel.transform.localPosition.z < 0)
+				wheel.motorTorque = torque;
+
+			// update visual wheels if any
+			if (wheelShape) 
+			{
+				Quaternion q;
+				Vector3 p;
+				wheel.GetWorldPose (out p, out q);
+
+				// assume that the only child of the wheelcollider is the wheel shape
+				Transform shapeTransform = wheel.transform.GetChild (0);
+				shapeTransform.position = p;
+				shapeTransform.rotation = q;
+			}
+
+		}
+	}
+}
